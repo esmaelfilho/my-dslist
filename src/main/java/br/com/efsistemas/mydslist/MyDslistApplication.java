caf9021b19f523cd64b88968fb5fda6c09f2fd47
Esmael A O Filho <esmaeloliveiracba@gmail.com>
2023-05-10 10:05:37 -0400

    COMMIT INICIAL

---
+++ src/main/java/br/com/efsistemas/mydslist/MyDslistApplication.java
@@ -0,0 +1,13 @@
+package br.com.efsistemas.mydslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/efsistemas/mydslist/MyDslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.efsistemas.mydslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
